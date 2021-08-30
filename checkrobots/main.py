90f8270ab936d1947639e51813bed8c00cb43587
Niklas Mikael <nikke.larsson@gmail.com>
2021-08-30 11:51:58 +0300

    Added verbose -functionality.

--- checkrobots/main.py
+++ checkrobots/main.py
@@ -44,32 +44,38 @@ def print_robots(robots: str, sort: bool=True) -> None:
         print(robots)
 
 
-def get_response(url: str) -> object:
+def get_response(url: str, bar: bool) -> object:
     """
     Fetch the response object, A.K.A the robots.txt.
 
     Parameters
     ----------
     url........ Get the robots.txt of this url.
+    bar........ Print an indicator of sorts to indicate something's happening.
     """
     try:
+        if (bar):
+            print("Trying to fetch robots.txt ...")
+            print("[Address]: {}".format(url))
         response: object = requests.get(url)
+        print() if (bar) else print(end="")
     except requests.ConnectionError as exception:
         sys.exit("{}: Error: Couldn't connect to {} ...".format(sys.argv[0], url))
     
     return response
 
 
-def print_all(url: str, headers: bool=False) -> None:
+def print_all(url: str, verbose: bool, headers: bool) -> None:
     """
     Print out all: the actual robots.txt, and additionally headers.
     
     Parameters
     ----------
     url.......... Use this url to fetch the robots.txt.
+    verbose...... Let the output be a little more verbose.
     headers...... Also print the headers in addition to the robots.txt.
     """
-    response: object = get_response(url)
+    response: object = get_response(url, bar=False if not (verbose) else True)
     if (headers): # Observing headers can offer user more info.
         print_headers(response.headers)
     print_robots(response.text)
@@ -77,6 +83,7 @@ def print_all(url: str, headers: bool=False) -> None:
 
 def main(args: list=sys.argv) -> None:
     headers: bool = False
+    verbose: bool = False
     args_checked: list = [] # List of arg names that are already checked.
     url_simple: str = ""
     url_formatted: str = ""
@@ -86,6 +93,9 @@ def main(args: list=sys.argv) -> None:
             continue
         if (arg == "-H") or (arg == "--include-headers"):
             headers = True
+            args_checked.append(arg)
+        elif (arg == "-v") or (arg == "--verbose"):
+            verbose = True
             args_checked.append(arg)
         if not(arg.startswith("-")):
             url_simple = arg
@@ -98,7 +108,11 @@ def main(args: list=sys.argv) -> None:
         else:
             sys.exit("{}: Info: '{}' not recognized ..".format(args[0], url_simple))
 
-        print_all(url_formatted, headers=False if not (headers) else True)
+        print_all(
+                url_formatted,
+                verbose=False if not (verbose) else True,
+                headers=False if not (headers) else True
+                )
 
 
 if (__name__ == "__main__"):
