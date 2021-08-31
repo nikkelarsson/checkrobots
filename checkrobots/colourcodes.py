5a586c23119deef03e84bfcdd89e8cfd66678b54
Niklas Mikael <nikke.larsson@gmail.com>
2021-08-31 15:14:41 +0300

    Created module for all different colorcodes.

---
+++ checkrobots/colourcodes.py
@@ -0,0 +1,27 @@
+# Styles
+NORMAL: str = "0"
+BOLD: str = "1"
+LIGHT: str = "2"
+ITALICIZED: str = "3"
+UNDERLINED: str = "4"
+BLINK: str = "5"
+
+# Foreground -colors.
+BLACK: str = "30"
+RED: str = "31"
+GREEN: str = "32"
+YELLOW: str = "33"
+BLUE: str = "34"
+PURPLE: str = "35"
+CYAN: str = "36"
+WHITE: str = "37"
+
+# Background -colors.
+BLACK: str = "40"
+RED: str = "41"
+GREEN: str = "42"
+YELLOW: str = "43"
+BLUE: str = "44"
+PURPLE: str = "45"
+CYAN: str = "46"
+WHITE: str = "47"
