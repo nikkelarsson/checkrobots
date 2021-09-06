805883265541c14481c422f77e0a3245aa4c803d
Niklas Mikael <nikke.larsson@gmail.com>
2021-09-06 12:09:57 +0300

    Improved func "gen_url".

--- checkrobots/urls.py
+++ checkrobots/urls.py
@@ -6,4 +6,23 @@ Date: 5.9.2021
 
 
 # Website names with their corresponding urls to the robots.txt.
-websites: dict = {"youtube": "https://www.youtube.com/robots.txt"}
+WEBSITES: dict = {
+        "youtube": "https://www.youtube.com/robots.txt",
+        "google": "https://www.google.fi/robots.txt"
+        }
+
+def gen_url(website: str) -> str:
+    """
+    Generate a url by looking at the website's name.
+
+    Parameters
+    ----------
+    website...... Website's name (google, youtube etc).
+    """
+    url: str = ""
+    for i in WEBSITES.items():
+        if (website == i[0]):
+            url = i[1]
+            break
+
+    return url
