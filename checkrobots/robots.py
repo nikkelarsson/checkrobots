07bfe323e2a00a65d589bfff8fa85f57e131d069
Niklas Mikael <nikke.larsson@gmail.com>
2021-09-07 09:49:50 +0300

    Gave better, shorter names to functions.
    - Also added docstring to a function.

--- checkrobots/robots.py
+++ checkrobots/robots.py
@@ -5,13 +5,16 @@ Date: September 7, 2021
 """
 
 
-def print_robots_allowed() -> None:
+def print_allowed(robots_txt: str, sort: bool) -> None:
     """Print fields from the robots.txt that begin with 'Allow'."""
     pass
 
 
-def print_robots_disallowed() -> None:
+def print_disallowed(robots_txt: str, sort: bool) -> None:
     """Print fields from the robots.txt that begin with 'Disallow'."""
     pass
 
 
+def print_all() -> None:
+    """Print all the fields from the robots.txt, both 'Allow' and 'Disallow'."""
+    pass
